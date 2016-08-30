9bb3d4353841a60ce587077da1a2a62aa734199c
tywang <tywang@thoughtworks.com>
2016-08-30 18:55:27 +0800

    add web2 to system

--- Gemfile
+++ Gemfile
@@ -4,4 +4,3 @@ gem "sinatra"
 gem "slim"
 gem "mongo"
 gem "json"
-gem 'rack', '1.6.2'
--- scripts/upload_app_to_env.sh
+++ scripts/upload_app_to_env.sh
@@ -3,7 +3,11 @@ rm -rf dist
 mkdir dist
 tar -cvf dist/myDoit.tar Gemfile Gemfile.lock views config.yml web.rb
 
-domain=192.168.56.102
-scp -o StrictHostKeychecking=no dist/myDoit.tar vagrant@$domain:/tmp
-scp -o StrictHostKeychecking=no scripts/deploy.sh vagrant@$domain:/tmp
-ssh -o StrictHostKeychecking=no vagrant@$domain 'sudo sh /tmp/deploy.sh'
+domain1=192.168.56.102
+domain2=192.168.56.102
+scp -o StrictHostKeychecking=no dist/myDoit.tar vagrant@$domain1:/tmp
+scp -o StrictHostKeychecking=no scripts/deploy.sh vagrant@$domain1:/tmp
+ssh -o StrictHostKeychecking=no vagrant@$domain1 'sudo sh /tmp/deploy.sh'
+scp -o StrictHostKeychecking=no dist/myDoit.tar vagrant@$domain2:/tmp
+scp -o StrictHostKeychecking=no scripts/deploy.sh vagrant@$domain2:/tmp
+ssh -o StrictHostKeychecking=no vagrant@$domain2 'sudo sh /tmp/deploy.sh'
