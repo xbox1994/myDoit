30960a180ef7d752490fec18ce22040223e72499
tywang <tywang@thoughtworks.com>
2016-08-30 19:19:38 +0800

    change doit path on web

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm -rf /usr/share/thin/myDoit
-mkdir -p /usr/share/thin/myDoit
-/bin/tar -xvf /tmp/myDoit.tar -C /usr/share/thin/myDoit
-cd /usr/share/thin/myDoit
+rm -rf ~/myDoit
+mkdir -p ~/myDoit
+/bin/tar -xvf /tmp/myDoit.tar -C ~/myDoit
+cd ~/myDoit
 killall ruby
 bundle install
 ruby web.rb &
