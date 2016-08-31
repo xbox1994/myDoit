183827c6cfc5980b25d1b42dc3a1c5611f2fe8c5
tywang <tywang@thoughtworks.com>
2016-08-31 21:41:31 +0800

    ..

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-
-rm -rf ~/myDoit
-mkdir -p ~/myDoit
-/bin/tar -xvf /tmp/myDoit.tar -C ~/myDoit
-cd ~/myDoit
-killall ruby
-bundle install
-ruby web.rb >/dev/null 2>&1 &
+echo -------------------------"$HOSTNAME"------------------------------
+if [ "$HOSTNAME" = "nginx" ]; then
+    nginx -s stop
+    nginx
+else
+    rm -rf ~/myDoit
+    mkdir -p ~/myDoit
+    /bin/tar -xvf /tmp/myDoit.tar -C ~/myDoit
+    cd ~/myDoit
+    killall ruby
+    bundle install
+    ruby web.rb >/dev/null 2>&1 &
+fi
--- scripts/upload_app_to_env.sh
+++ scripts/upload_app_to_env.sh
@@ -5,7 +5,7 @@ tar -cvf dist/myDoit.tar Gemfile Gemfile.lock views config.yml web.rb
 
 domain1=192.168.56.101
 domain2=192.168.56.102
-domain2=192.168.56.103
+domain3=192.168.56.103
 scp -o StrictHostKeychecking=no dist/myDoit.tar vagrant@$domain1:/tmp
 scp -o StrictHostKeychecking=no scripts/deploy.sh vagrant@$domain1:/tmp
 ssh -o StrictHostKeychecking=no vagrant@$domain1 'sudo sh /tmp/deploy.sh'
