71518a7415abd000ff54216e96cb26eb824e37d6
WangTianyi <tywang@thoughtworks.com>
2017-12-10 00:54:40 +0800

    update grunt

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+npm i
 grunt build
+
 rm -rf dist
 mkdir dist
 tar -cvf dist/myDoit.tar Gemfile Gemfile.lock views config.yml web.rb
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,5 @@ echo $USER
 pwd
 
 domain2=192.168.56.102
-scp -o StrictHostKeychecking=no dist/myDoit.tar vagrant@$domain2:/tmp
-scp -o StrictHostKeychecking=no scripts/deploy.sh vagrant@$domain2:/tmp
-ssh -o StrictHostKeychecking=no vagrant@$domain2 'sudo sh /tmp/deploy.sh'
+scp -o StrictHostKeychecking=no dist/myDoit.tar scripts/deploy.sh vagrant@${domain2}:/tmp
+ssh -o StrictHostKeychecking=no vagrant@${domain2} 'sudo sh /tmp/deploy.sh'
