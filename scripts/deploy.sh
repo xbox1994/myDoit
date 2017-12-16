df0099dee8922a3032513d221236b203ec78f9f9
WangTianyi <tywang@thoughtworks.com>
2017-12-16 22:51:55 +0800

    test jenkins

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 domain2=192.168.56.102
-
-echo $USER
-pwd
-
-echo ${domain2}
-ping ${domain2}
-
-scp -o StrictHostKeychecking=no dist/myDoit.tar scripts/deploy.sh vagrant@${domain2}:/tmp
-ssh -o StrictHostKeychecking=no vagrant@${domain2} 'sudo sh /tmp/deploy.sh'
+scp -o StrictHostKeychecking=no dist/myDoit.tar scripts/run.sh vagrant@${domain2}:/tmp
+ssh -o StrictHostKeychecking=no vagrant@${domain2} 'sudo bash /tmp/run.sh'
